ec6168d30f2ee00d9be459bb5d11460945adb612
saoutarrih <osselosse14@hotmail.com>
2021-09-29 10:00:17 +0200

    adaptation

--- src/main/java/com/solution/productdiscount/ProductDiscountApplication.java
+++ src/main/java/com/solution/productdiscount/ProductDiscountApplication.java
@@ -3,11 +3,14 @@ package com.solution.productdiscount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProductDiscountApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductDiscountApplication.class, args);
+		
 	}
+	
 
 }
